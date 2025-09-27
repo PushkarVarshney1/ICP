4434e42a2267f0b3c80c810bfbaa475e4fe292f9
PushkarVarshney1 <varshneypushkar01@gmail.com>
2025-09-27 18:21:21 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (52.44%) - LeetSync

---
+++ Assignment-2/367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num == 1)return true;
+        long lo=1;
+        long hi=num/2; // b/c iteger is always less than or equal to half of its perfect square
+        while(lo <= hi){
+            long mid=lo+(hi-lo)/2;
+            if(mid * mid == num)return true;
+            else if(mid * mid < num)lo = mid+1;
+            else hi=mid-1;
+        }
+        return false;
+    }
+}

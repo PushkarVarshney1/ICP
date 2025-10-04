b0b6af4c3efceebbb745894dd1193474f710bca9
PushkarVarshney1 <varshneypushkar01@gmail.com>
2025-10-04 21:51:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (36.18%) - LeetSync

--- Assignment-6/198-house-robber/house-robber.java
+++ Assignment-6/198-house-robber/house-robber.java
@@ -6,11 +6,11 @@ class Solution {
     }
     // Top Down Approach
     public int Robber(int[] arr,int i,int[]dp){
-        if(i>=arr.length)return 0;
-        if(dp[i] != -1)return dp[i];
+        if(i >= arr.length) return 0;
+        if(dp[i] != -1) return dp[i];
 
-        int Rob=arr[i]+Robber(arr,i+2,dp);
-        int no_Rob=Robber(arr,i+1,dp);
+        int Rob = arr[i] + Robber(arr,i+2,dp);
+        int no_Rob = Robber(arr,i+1,dp);
 
         return dp[i]=Math.max(Rob,no_Rob);
     }

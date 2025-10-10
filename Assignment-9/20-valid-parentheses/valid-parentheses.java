5356d99133081146b6b26fff9a8ec4570fea0826
PushkarVarshney1 <varshneypushkar01@gmail.com>
2025-10-10 12:08:51 +0530

    Time: 3 ms (54.47%) | Memory: 41.9 MB (69.00%) - LeetSync

--- Assignment-9/20-valid-parentheses/valid-parentheses.java
+++ Assignment-9/20-valid-parentheses/valid-parentheses.java
@@ -2,21 +2,18 @@ class Solution {
     public boolean isValid(String s) {
         int n=s.length();
         if(n==1)return false;
-        char ch=s.charAt(0);
-        // if(ch == ')' || ch=='}' || ch==']')return false;
         Stack<Character> st=new Stack<>();
 
         for(int i=0; i<s.length(); i++){
-            ch=s.charAt(i);
+            char ch=s.charAt(i);
             if(ch == '(' || ch=='{' || ch=='[')st.push(ch);
             else{
                 if(st.isEmpty())return false;
-                else if(st.peek()=='(' && ch==')')st.pop();
-                else if(st.peek()=='[' && ch==']')st.pop();
-                else if(st.peek()=='{' && ch=='}')st.pop();
+                else if((st.peek()=='(' && ch==')') || (st.peek()=='[' && ch==']')||(st.peek()=='{' && ch=='}') )st.pop();
                 else return false;
             }
         }
+        // not return only true b/c all brackets are open
         return st.isEmpty();
     }
 }

5b0cb9d365be8ed7ac4233ee744284d152a0dafe
PushkarVarshney1 <varshneypushkar01@gmail.com>
2025-10-25 00:35:52 +0530

    Time: 27 ms (67.27%) | Memory: 45.4 MB (79.85%) - LeetSync

---
+++ Assignment-10/1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
@@ -0,0 +1,38 @@
+class Solution {
+    static class Pair {
+        char ch;
+        int count;
+        Pair(char ch, int count) {
+            this.ch = ch;
+            this.count = count;
+        }
+    }
+    public String removeDuplicates(String s, int k) {
+        // Stack of pairs: [character, count]
+        Stack<Pair> st = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            // If same as previous char, increment count
+            if (!st.isEmpty() && st.peek().ch == ch) {
+                st.peek().count++;
+
+                // When count reaches k, pop it (remove those k duplicates)
+                if (st.peek().count == k) {
+                    st.pop();
+                }
+            } else {
+                // new char starts with count = 1
+                st.push(new Pair(ch, 1));
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Pair p : st) {
+            for (int i = 0; i < p.count; i++) {
+                sb.append(p.ch);
+            }
+        }
+        return sb.toString();
+    }
+
+    
+}

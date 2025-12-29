b892336e9987a85e64287ccf8f7048e47e0e6531
PushkarVarshney1 <varshneypushkar01@gmail.com>
2025-12-29 22:39:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (39.56%) - LeetSync

--- Assignment-10/1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ Assignment-10/1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,28 +1,15 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        List<Integer> ll = new ArrayList<>();
-        return fun(nums,0,ll,0);
+        return fun(nums,0,0);
     }
-    public int fun(int []nums , int idx, List<Integer> ll,int ans){
+    public int fun(int []nums , int idx, int ans){
         if(idx >= nums.length){
-            if(ll.size() == 1)ans+=ll.get(0);
-            else {
-                int XOR = 0;
-                for(int a : ll){
-                    XOR ^= a;
-                }
-                ans+=XOR;
-            }
             return ans;
-
         }
         // Don't add
-        int NOTinclude = fun(nums, idx +1, ll,ans);
+        int NOTinclude = fun(nums, idx +1, ans );
         // Add
-        int x = nums[idx];
-        ll.add(x);
-        int Include = fun(nums, idx+1, ll, ans);
-        ll.remove(ll.size()-1);
+        int Include = fun(nums, idx+1, ans ^ nums[idx]);
 
         return Include + NOTinclude;
     }

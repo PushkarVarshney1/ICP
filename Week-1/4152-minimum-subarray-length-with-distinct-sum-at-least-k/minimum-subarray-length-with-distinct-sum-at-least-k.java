67428dcac05c5a2cba6db5b05ca0fcde25f4924d
PushkarVarshney1 <varshneypushkar01@gmail.com>
2026-01-18 22:44:48 +0530

    Time: 143 ms (40.33%) | Memory: 169.1 MB (64.92%) - LeetSync

---
+++ Week-1/4152-minimum-subarray-length-with-distinct-sum-at-least-k/minimum-subarray-length-with-distinct-sum-at-least-k.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minLength(int[] nums, int k) {
+                         // Sliding Window 
+            
+        int n = nums.length;
+        int ans = Integer.MAX_VALUE;
+        HashMap<Integer,Integer> map = new HashMap<>(); // to tell distiction for the elements
+        int si = 0 , ei = 0;
+        int sum = 0;
+        while(ei < n){
+            map.put(nums[ei] , map.getOrDefault(nums[ei] , 0)+1);
+            if(map.get(nums[ei]) == 1)sum += nums[ei];
+            // shrinking
+            while(sum >= k){
+                ans = Math.min(ans , ei - si + 1); // ans updation
+                map.put(nums[si] , map.get(nums[si]) - 1);
+                if(map.get(nums[si]) == 0) sum -= nums[si]; 
+                si++;
+            }
+            ei++;
+        }
+        return (ans == Integer.MAX_VALUE) ? -1 : ans;
+        
+        // TC or SC-> O(n)
+    }
+}

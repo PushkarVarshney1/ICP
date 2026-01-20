7768de152b023869013d5ba451f75332dc85f739
PushkarVarshney1 <varshneypushkar01@gmail.com>
2026-01-20 14:15:48 +0530

    Time: 9 ms (16.79%) | Memory: 45 MB (17.95%) - LeetSync

---
+++ Week-2/1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        Stack<Character> st=new Stack<>();
+        StringBuilder sb=new StringBuilder();
+        for(Character ch:s.toCharArray()){
+            if(ch == '('){
+                if(st.size() > 0) sb.append(ch);
+                st.push(ch);
+            }
+            else{
+                st.pop();
+                if(st.size() > 0) sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
